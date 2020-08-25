6b4a358e77f08dc9dfdc307edcdf599195fe91cb
Киселев Михаил В <mvkiselev@group.legion.ru>
2020-08-25 16:25:06 +0300

    Version 20200825 16:24

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const path = require('path')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-const routesCourses = require('./routers/courses.js')
-const routesStreams = require('./routers/streams.js')
-const routesUsers = require('./routers/users.js')
+const routersCourses = require('./routers/courses.js')
+const routersStreams = require('./routers/streams.js')
+const routersUsers = require('./routers/users.js')
 
 const app = express()
 
@@ -13,8 +13,8 @@ app.use(express.static(path.join(__dirname, '/')))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.use('/courses', routesCourses)
-app.use('/streams', routesStreams)
-app.use('/users', routesUsers)
+app.use('/courses', routersCourses)
+app.use('/streams', routersStreams)
+app.use('/users', routersUsers)
 
 app.listen(5000, () => console.log('Running on Port 5000'))
